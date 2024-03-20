c7667a929beaa25dc4a72f0e00341646881cf421
bensalah abderrahman <bensalahabdelrrahman1@gmail.com>
2024-03-20 17:22:05 +0100

    v1 ui

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import { Karla } from 'next/font/google'
+import './globals.css'
+import { SideBar } from '../components/sidebar';
+import Header from '../components/header';
+import PageWrapper from '../components/pagewrapper';
+import { ThemeProvider } from '../components/theme-provider';
+import { Metadata } from 'next';
+
+const karla = Karla({
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  subsets: ['latin'],
+  variable: "--font-karla"
+})
+export const metadata: Metadata = {
+  title: "Dz admin dashboard",
+  description: "NextJs admin dashboard created by Sijin Raj"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={karla.className + ' h-screen overflow-hidden'}>
+        <ThemeProvider
+          themes={['dark', 'custom', 'light']}
+          attribute="class"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <>
+            <SideBar />
+            <div className="flex flex-col h-full w-full">
+              <Header />
+              <PageWrapper children={children} />
+            </div>
+          </>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
